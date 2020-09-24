4f727c90ebad414944a3c8199359fee3c1bc7611
akam alam <arundelfootball23@gmail.com>
2020-09-24 19:02:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,10 +10,10 @@ def read_from_hash(hash, key)
 
 def update_counting_hash(hash, key)
   if hash[key] 
-  
-  
-  
-  new_hash_update = 
+  hash[key] = hash[key] +=1 
+else 
+  hash[key] = 1 
+  new_hash_update = hash[key] 
   
   
   # given a hash an a key as parameters, return an updated hash
