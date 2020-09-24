9d47e48f1520faac5d08f1357d44489fd65542b2
akam alam <arundelfootball23@gmail.com>
2020-09-24 18:53:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-    hash{key}
-end
+    hash[1]
+    end
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
